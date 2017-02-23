bc4e3d309fd787b9768fdc0f8fe99c0fde2675db
vharnach <vharnach@gmail.com>
2017-02-23 15:35:10 +0100

    xD

--- express-mongoose-dev/app.js
+++ express-mongoose-dev/app.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/presentations', presentations);
+app.use('/presentations/:id', presentations);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
